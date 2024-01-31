d646f534e05e128385aa98346c7aa04efdf3fc54
José Felipe <132314205+fel1pee3@users.noreply.github.com>
2024-01-31 15:00:05 -0300

    versão computador finalizado

---
+++ script.js
@@ -0,0 +1,82 @@
+const latap1=document.querySelector(".latap1")
+const latap2=document.querySelector(".latap2")
+const latap3=document.querySelector(".latap3")
+const latap4=document.querySelector(".latap4")
+const latap5=document.querySelector(".latap5")
+const latap6=document.querySelector(".latap6")
+const latap7=document.querySelector(".latap7")
+const latap8=document.querySelector(".latap8")
+const latap9=document.querySelector(".latap9")
+const latap10=document.querySelector(".latap10")
+const latap11=document.querySelector(".latap11")
+const latap12=document.querySelector(".latap12")
+const latap13=document.querySelector(".latap13")
+const latag=document.querySelector(".lataG")
+const info_sabor=document.querySelector(".sabor-info")
+const nome_sabor=document.querySelector(".sabor")
+
+latap1.addEventListener("click", ()=>{
+    latag.src="img/monster-original.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY ORIGINAL"
+    info_sabor.innerHTML="Surpreenda-se com a lata do energético mais animal do planeta. Com duas vezes mais energia do que uma bebida energética regular. Monster possui uma presença marcante e um sabor único! Inconfundível."
+})
+latap2.addEventListener("click", ()=>{
+    latag.src="img/monster-absolutelyzero.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY ABSOLUTELY ZERO"
+    info_sabor.innerHTML="Monster Energy Absolutely Zero traz o mesmo sabor e o mesmo punch energético do original, com menos calorias e sem açúcares."
+})
+latap3.addEventListener("click", ()=>{
+    latag.src="img/monster-ultraparadise.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY ULTRA PARADISE"
+    info_sabor.innerHTML="Monster Ultra Paradise é puro, refrescante e revigorante como os sabores da ilha. O paraíso está logo ali!"
+})
+latap4.addEventListener("click", ()=>{
+    latag.src="img/monster-zeroultra.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY ZERO ULTRA"
+    info_sabor.innerHTML="Mais refrescante. Mais leve. Monster Energy Zero Ultra é zero açúcar, zero calorias e uma carga completa de nossa mistura de energia Monster."
+})
+latap5.addEventListener("click", ()=>{
+    latag.src="img/monster-ultraviolet.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY ULTRA VIOLET"
+    info_sabor.innerHTML="Inspirado nos anos 70. Época de atitude livre e positiva, com um estilo único e inconfundível, tempo onde o psicodélico, glamour, heavy metal e rock se misturaram em uma harmonia única."
+})
+latap6.addEventListener("click", ()=>{
+    latag.src="img/monster-ultrawatermelo.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY ULTRA WATERMELON"
+    info_sabor.innerHTML="Verão na lata para você aproveitar a qualquer momento. Agite aquelas noites quentes com Ultra Watermelon. Zero açúcar, fácil de beber e super refrescante."
+})
+latap7.addEventListener("click", ()=>{
+    latag.src="img/monster-juicekhaotic.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY JUICE KHAOTIC"
+    info_sabor.innerHTML="De volta a 2005, Monster Juice Original abalou o jogo, combinando um delicioso suco de verdade e atitude de bebida energética. Agora, mais de uma década depois, decidimos que era hora de deixar esse clássico ainda mais irado. Melhoramos o sabor e ajustamos o mix de suco para criar um sabor ainda melhor."
+})
+latap8.addEventListener("click", ()=>{
+    latag.src="img/monster-juicepipelinepunch.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY JUICE PIPELINE PUNCH"
+    info_sabor.innerHTML="Banzai Pipeline, a onda mais famosa do mundo no lendário North Shore de Oahu, ganha vida por apenas alguns breves meses do ano. Em homenagem a essa força épica da natureza, criamos Juice Monster Pipeline Punch. A combinação perfeita dos melhores sabores que o Havaí oferece - maracujá, laranja e goiaba - depois Monsterizado com o nosso famoso combo energético. E assim como a onda que lhe deu o nome, está destinado a se tornar uma lenda!"
+})
+latap9.addEventListener("click", ()=>{
+    latag.src="img/monster-juicekhaos.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY JUICE KHAOS"
+    info_sabor.innerHTML="Começamos com o sabor original, adicionando uma combinação agressiva de sucos naturais, em seguida carregamos essa combinação com uma carga de energia e está criado! Insano e híbrido! Parte suco natural, 100% Monster."
+})
+latap10.addEventListener("click", ()=>{
+    latag.src="img/monster-juicepacificpunch.webp"
+    nome_sabor.innerHTML="MONSTER ENERGY JUICE PACIFIC PUNCH"
+    info_sabor.innerHTML="Com um sabor profundo e amplo, assim como o Oceano Pacífico, e com um mix de frutas nada tradicional inspirado nos clássicos ponches. Pacific Punch traz a energia Monster e chega para te deixar com vento em popa e pronto para encarar até as marés mais agitadas. O ponche de frutas naturais em sua versão mais irada!" 
+})
+latap11.addEventListener("click", ()=>{
+    latag.src="img/monster-juicemangoloco.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY JUICE MANGO LOCO"
+    info_sabor.innerHTML="Inspirado pelas tradições mexicanas e com um mix de frutas com manga. Além de sabor também entrega energia!"
+})
+latap12.addEventListener("click", ()=>{
+    latag.src="img/monster-dragonicetealimao.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY DRAGON ICE TEA LIMÃO"
+    info_sabor.innerHTML="Energético em formato de chá, sem gás com leve toque de limão. Inspirado em tradições milenares da china"
+})
+latap13.addEventListener("click", ()=>{
+    latag.src="img/monster-dragoniceteapessego.webp" 
+    nome_sabor.innerHTML="MONSTER ENERGY DRAGON ICE TEA PÊSSEGO"
+    info_sabor.innerHTML="Energético em formato de chá, sem gás com leve toque de pêssego. Inspirado em tradições milenares da china."
+})
